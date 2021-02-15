d5c2a7f3273ca1382130aad95b0b305203e1597c
duran <duran750@hotmail.com>
2021-02-15 12:48:11 -0300

    edit ci

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
-  region  = "us-east-1"
-  profile = "particular"
+  region     = "us-east-1"
+  profile    = "particular"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+
 }
 
 terraform {
