7bed4dc51e2e09f086a48126eaafe098ffcac314
duran <duran750@hotmail.com>
2021-02-15 20:27:05 -0300

    getip api

--- iac/terraform/modules/vars.tf
+++ iac/terraform/modules/vars.tf
@@ -4,12 +4,12 @@ variable "aws_ami" {
 
 variable "instance_type_master" {
   #default = "t2.micro"
-  default = "t2.medium"
+  default = "t2.large"
 }
 
 variable "instance_type_node" {
   #default = "t2.micro"
-  default = "t2.medium"
+  default = "t2.large"
 }
 
 variable "instance_count_master" {
