9e79255890e549135cf7ab364a598ae494fef052
duran <duran750@hotmail.com>
2021-02-15 12:56:27 -0300

    edit aws secret

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region     = "us-east-1"
   profile    = "particular"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  #access_key = var.AWS_ACCESS_KEY_ID
+  #secret_key = var.AWS_SECRET_ACCESS_KEY
 
 }
 
