d94659f0585018137a6e02fa6044dcb1d9791d42
duran <duran750@hotmail.com>
2021-02-15 22:39:38 -0300

    hpa

--- api/stress.sh
+++ api/stress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..10000}; do
-  curl http://simplecrud.com/docs
+  curl http://192.168.124.134:30000/docs
   sleep $1
 done
--- api/tests.py
+++ api/tests.py
@@ -1,12 +1,19 @@
 import requests
+import json
+import names
 
-url = "http://simplecrud.com/users"
+url = "http://192.168.125.134:30000/users"
 
-payload = '{"email":"fabio5","password":"ifood"}'
-headers = {'Content-Type': 'application/json'}
+while True:
+    name = names.get_full_name()
+    payload = {"email":name,"password":"ifood"}
+    headers = {'Content-Type': 'application/json'}
+    datajson = json.dumps(payload)
+    try:
+        response = requests.request("POST", url, data=datajson, headers=headers)
+        print(response.text)
+    except:
+        response = requests.request("GET", url, data=datajson, headers=headers)
+        print(response.text)
+    #print(response.text)
 
-response = requests.request("POST", url, data=payload, headers=headers)
-
-print(response.text)
-
-#def insert_users()
