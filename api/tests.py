d7b0ae8b1537ef492fb8d1177ddca5d30168342d
duranlopes <duran750@hotmail.com>
2021-04-13 23:47:48 -0300

    add asyncio

--- api/tests.py
+++ api/tests.py
@@ -1,17 +1,22 @@
 import requests
 import json
 import names
+import asyncio
 
-url = "http://54.83.190.198:30000/users"
+url = "http://10.99.232.63/users"
 
-while True:
-    name = names.get_full_name()
-    payload = {"email":name,"password":"ifood"}
-    headers = {'Content-Type': 'application/json'}
-    datajson = json.dumps(payload)
-    try:
-        response = requests.request("POST", url, data=datajson, headers=headers)
-        print(response.text)
-    except:
-        response = requests.request("GET", url+"?skip=0&limit=100000", data=datajson, headers=headers)
-        print(response.text)
+async def main():
+    while True:
+        name = names.get_full_name()
+        payload = {"email":name,"password":"ifood"}
+        headers = {'Content-Type': 'application/json'}
+        datajson = json.dumps(payload)
+        try:
+            response = requests.request("POST", url, data=datajson, headers=headers)
+            print(response.text)
+        except:
+            response = requests.request("GET", url+"?skip=0&limit=100000", data=datajson, headers=headers)
+            print(response.text)
+
+loop = asyncio.get_event_loop()  
+loop.run_until_complete(main()) 
